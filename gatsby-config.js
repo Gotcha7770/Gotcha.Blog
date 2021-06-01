89534661c4fe91f0486a172826feaf7c286d39b5
Gotcha7770 <gbakanov@inbox.ru>
2021-06-01 11:20:03 +0300

    add typography.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,6 +25,12 @@ module.exports = {
         path: `${__dirname}/src/blog/`,
       },
     },
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
     `gatsby-transformer-remark`,
   ],
 }
