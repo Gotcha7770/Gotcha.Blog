454ed87a0b93fa88f65571d8ae901168c313589c
Gotcha7770 <gbakanov@inbox.ru>
2021-06-01 18:19:14 +0300

    footer styling

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,9 +1,19 @@
 import * as React from 'react'
+import { wrapper } from './footer.module.css'
 
 const Footer = () => {
-    return (
-      <h1>Gotcha (c)</h1>
-    )
-  }
+  return (
+    <footer>
+      <div className={wrapper}>
+        <div>
+          <p>© 2021 by Gotcha. All rights reserved</p>
+        </div>
+        <div>
+          <p>built with Gatsby, delivered by Netlify</p>
+        </div>
+      </div>
+    </footer>
+  )
+}
 
 export default Footer
