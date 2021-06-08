b0453f1f1a14a4ab6c420ec78b117760a582575b
Gotcha7770 <gbakanov@inbox.ru>
2021-06-08 17:23:09 +0300

    blog post layout

--- src/components/blog-post.jsx
+++ src/components/blog-post.jsx
@@ -2,6 +2,12 @@ import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import {
+  heroContainer,
+  hero,
+  emojiContainer,
+  emoji
+} from "./blog-post.module.css"
 
 export default function BlogPost({ data }) {
   const post = data.markdownRemark
@@ -10,8 +16,13 @@ export default function BlogPost({ data }) {
 
   return (
     <Layout>
-      <div>
-        <GatsbyImage image={image} alt="post title image" />
+      <div className={heroContainer}>
+        <GatsbyImage image={image} className={hero} alt="post title image" />
+      </div>
+      <div className={emojiContainer}>
+        <span className={emoji} role="image" aria-label={post.frontmatter.icon}>
+          {post.frontmatter.icon}
+        </span>
       </div>
       <h1>{post.frontmatter.title}</h1>
       <small>{post.frontmatter.date}</small>
@@ -25,13 +36,14 @@ export const query = graphql`
     markdownRemark(fields: { slug: { eq: $slug } }) {
       html
       frontmatter {
+        icon
         title
-        date
+        date(formatString: "MMMM DD, yyyy")
       }
     }
     image: file(relativeDirectory: {eq: $pageName}, name: {eq: "hero"}) {
       childImageSharp {
-        gatsbyImageData(layout: FULL_WIDTH)
+        gatsbyImageData(layout: CONSTRAINED)
       }
     }
   }
