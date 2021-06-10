ab9c0fd9a16b537b3ee780533ca6673e60cc68dd
Gotcha7770 <gbakanov@inbox.ru>
2021-06-10 17:51:29 +0300

    add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,6 +32,27 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "UA-168680569-2", // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          anonymize_ip: true,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+        },
+      },
+    },
+    {
       resolve: `gatsby-transformer-remark`,
       options: {
         // GitHub Flavored Markdown mode (default: true)
